0fe378f4bc0107678b59d85eacd3214477acf667
Échilley <echilleypereirasilva@gmail.com>
2025-05-16 19:47:43 -0300

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,26 @@
+/*
+Instruções:
+Crie um HTML básico e inclua o link externo para o arquivo script.js
+
+No JavaScript:
+Crie duas variáveis com números e exiba a soma delas no console
+
+Mostre no console.log() uma mensagem assim:
+console.log("A soma é:", soma);
+
+Use:
+let
+console.log()
+
+*/
+alert("voce quer consultar seu resultado ?")
+console.log("cheque seu resultado!")
+
+
+let num1 = 3
+let num2 = 2
+resposta = num1+num2
+
+console.log("o resutado é " + resposta );
+
+
